6b26114868fbae96a37da4b6b44e567dc57516fe
Olga_ <olga_art1025@mail.ru>
2022-04-06 22:42:56 +0500

    7. HTML form. Создание вакансии. [#504844]

--- src/main/java/ru/job4j/dreamjob/control/PostController.java
+++ src/main/java/ru/job4j/dreamjob/control/PostController.java
@@ -3,7 +3,10 @@ package ru.job4j.dreamjob.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
+
+import java.time.LocalDate;
 
 @Controller
 public class PostController {
@@ -14,5 +17,12 @@ public class PostController {
     public String posts(Model model) {
         model.addAttribute("posts", store.findAll());
         return "posts";
+    }
+
+    @GetMapping("/addPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post(0, "Заполните название вакансии",
+                "Заполните описание", LocalDate.now()));
+        return "addPost";
     }
 }
